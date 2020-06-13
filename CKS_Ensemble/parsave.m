eb6210642b9b12d2f1ab3d64e0d921647e96501e
clement etienam <39471613+clementetienam@users.noreply.github.com>
2020-06-13 17:27:41 +0100

    Add files via upload

--- CKS_Ensemble/parsave.m
+++ CKS_Ensemble/parsave.m
@@ -1,8 +1,9 @@
-function parsave(hardanswer,softanswer,weights_updated,modelNN,Class_all,clfy)
+function parsave(hardanswer,softanswer,weights_updated,modelNN,Class_all,clfy,datass)
 save('predict_hard.out','hardanswer','-ascii')
 save('predict_soft.out','softanswer','-ascii')
 save('GP.mat', 'weights_updated');
 save('Classifier.mat', 'modelNN');
 save('clfy.mat', 'clfy');
 save('Class_all.mat', 'Class_all');
+save('Dataused.out','datass','-ascii')
 end
